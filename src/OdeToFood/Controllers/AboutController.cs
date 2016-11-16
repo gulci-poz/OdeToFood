ce97cec5d543995b6cbd749b6e41ab890e3886ba
gulci-poz <sebastian.gulczynski.poznan@gmail.com>
2016-11-16 23:43:27 +0100

    attribute routes

--- src/OdeToFood/Controllers/AboutController.cs
+++ src/OdeToFood/Controllers/AboutController.cs
@@ -1,17 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace OdeToFood.Controllers
 {
+    // attribute routes
+    //[Route("about")]
+
+    // z użyciem tokenów w []
+    // podając token kontrolera i akcji, nie potrzebujemy atrybutu Route przy metodach
+
+    // możemy używać literałów
+
+    [Route("[controller]/[action]")]
     public class AboutController
     {
+        // domyślna akcja, w url wystarczy samo about
+        //[Route("")]
+
+        //[Route("[action]")]
         public string Phone()
         {
             return "1+555-555-5555";
         }
 
+        //[Route("address")]
+
+        //[Route("[action]")]
         public string Address()
         {
             return "USA";
