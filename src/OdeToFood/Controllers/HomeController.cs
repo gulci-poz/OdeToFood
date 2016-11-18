878d91c3b28bd8dc60273d359c0479abd5a261d4
gulci-poz <sebastian.gulczynski.poznan@gmail.com>
2016-11-18 07:23:44 +0100

    returning IActionResult

--- src/OdeToFood/Controllers/HomeController.cs
+++ src/OdeToFood/Controllers/HomeController.cs
@@ -1,10 +1,27 @@
-﻿namespace OdeToFood.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using OdeToFood.Models;
+
+namespace OdeToFood.Controllers
 {
-    public class HomeController
+    public class HomeController : Controller
     {
-        public string Index()
+        // jeśli zwracamy tylko jeden typ rezultatu,
+        // to warto użyć konkretnej klasy zamiast IAction Result,
+        // pomoże to w testowaniu unit test
+
+        // stosowanie kontrolera bazowego Controller oraz interfejsu IActionResult
+        // daje nam elastyczność oddzielenia decyzji o akcji i samej akcji
+
+        public IActionResult Index()
         {
-            return "Hello from the HomeController!";
+            // ewentualnie ContentResult
+            //return Content("Hello from the HomeController!");
+
+            var model = new Restaurant { Id = 1, Name = "The House of gulci" };
+
+            // domyślne ustawienie mvc, obiekt będzie serializowany do json
+            // ObjectResult daje nam content negotiation, możemy wybrać w jaki sposób zwrócimy dane
+            return new ObjectResult(model);
         }
     }
 }
